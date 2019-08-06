9b6169e299afd265f8e69df78615294bc2f845c2
WouterMoedt <wouter.moedt@live.nl>
2019-08-06 11:33:16 +0200

    Changes for Heroku2

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.utils import timezone
-
+from django.contrib.auth.models import User
 
 class Post(models.Model):
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
@@ -17,4 +17,3 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
-
